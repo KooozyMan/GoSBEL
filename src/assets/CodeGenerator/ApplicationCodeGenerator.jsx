59c7b30a3071c8cfd2f2a8559658494f53596416
KooozyMan <90005746+KooozyMan@users.noreply.github.com>
2026-03-19 08:49:28 +0300

    fixed bad package

--- src/assets/CodeGenerator/ApplicationCodeGenerator.jsx
+++ src/assets/CodeGenerator/ApplicationCodeGenerator.jsx
@@ -1,5 +1,5 @@
 export default function ApplicationCodeGenerator(baseArtifact = `Demo`, basePackage = `com.example`) {
-    const code = `package ${basePackage}.${baseArtifact};
+    const code = `package ${basePackage};
     
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
